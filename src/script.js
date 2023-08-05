8761a5a17bcc75e4f6de0788489a6d3e8395e525
Yash Gupta <yashgupta27690@gmail.com>
2023-08-05 19:02:06 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -4,13 +4,13 @@ const getIpbtn = document.getElementById('btn')
 
 
 
-getIpbtn.addEventListener('click',()=>{
+
     fetch("https://api.ipify.org?format=json")
 .then(response=> response.json())
 .then(data=> {ipAddressbox.innerText=`Your IP Adress is: ${data.ip}`;sessionStorage.setItem('ip' , data.ip) })
 .catch(err=> console.log(err))
 
-
+getIpbtn.addEventListener('click',()=>{
 
     window.location.href='./src/home.html'
 })
