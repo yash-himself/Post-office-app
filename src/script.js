efb95ea4829f5a20e5576b2fcfdd81de3cda9f38
Yash Gupta <yashgupta27690@gmail.com>
2023-08-05 18:43:06 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -5,18 +5,15 @@ const getIpbtn = document.getElementById('btn')
 
 
 getIpbtn.addEventListener('click',()=>{
-    navigator.geolocation.getCurrentPosition(position =>{
-        const {latitude, longitude} = position.coords;
-        sessionStorage.setItem('location', JSON.stringify([latitude,longitude]))
-    })
-    window.location.href='./src/home.html'
-})
-
-//ip address 
-fetch("https://api.ipify.org?format=json")
+    fetch("https://api.ipify.org?format=json")
 .then(response=> response.json())
 .then(data=> {ipAddressbox.innerText=`Your IP Adress is: ${data.ip}`;sessionStorage.setItem('ip' , data.ip) })
 .catch(err=> console.log(err))
+
+
+
+    window.location.href='./src/home.html'
+})
 
 
 
